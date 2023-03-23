58cd28c457c80d19857ad789585c40347dd18516
kevin otieno <kelvinotix@gmail.com>
2023-03-23 03:02:18 +0300

    completed the task

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,9 @@
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data.jsx';
 function App() {
   return (
     <div className='container'>
       <h2>Advanced React</h2>
+      <Starter/>
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,45 @@
+import {useState,useEffect} from 'react'
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const[users,setUsers] = useState([])
+  useEffect(()=>{
+    const fetchData =  async ()=>{
+      try{
+        const response = await fetch(url)
+        let users = await response.json()
+        setUsers(users)
+        console.log(users);
+      }
+      catch(err){
+        console.log(err);
+      }
+      
+    }
+    fetchData()
+  },[])
+  return( 
+    <div>
+    <section>
+    <h3>github users</h3>
+    <ul className='users'>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <li key={id}>
+            <img src={avatar_url} alt={login} />
+            <div>
+              <h5>{login}</h5>
+              <a href={html_url}>profile</a>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+    </div>
+    
+    
+    );
 };
 export default FetchData;
