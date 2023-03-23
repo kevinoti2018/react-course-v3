bba3acfcb34228335fff732fe4dc93e2a083f229
kevin otieno <kelvinotix@gmail.com>
2023-03-24 01:44:21 +0300

    finihed challenge

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,41 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const[user,setUser] = useState(null)
+  const[isLoading,setIsLoading] = useState(true)
+  const[isError,setIsError] = useState(false)
+  useEffect(()=>{
+    const fetchData = async()=>{
+     try{
+      const response = await fetch(url)
+      if(!response.ok){
+        setIsError(true)
+        setIsLoadin(false)
+        return
+      }
+      const user = await response.json()
+      setUser(user)
+      console.log(user);
+     }
+     catch(err){
+      console.log(err);
+      setIsError(err)
+     }
+     setIsLoading(false)
+    }
+    fetchData()
+  },[])
+  if(isLoading){
   return <h2>Fetch Data </h2>;
+}
+  if(isError){
+  return <h2>Error occured </h2>;}
+  return(
+    <div style={{width:'400px', height:'auto', padding:'10px',borderRadius:'10px', backgroundColor:'lightblue', display:'flex',flexDirection:'column'}}>
+    <img src={user.avatar_url} alt='user image' style={{width:'100px', borderRadius:'50px'}} />
+    <p>{user.company}</p>
+    <p>{user.bio}</p>
+    </div>
+  )
 };
 export default MultipleReturnsFetchData;
