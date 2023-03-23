f02ee93f3a1bf89dadd64859464c16e4c711b423
kevin otieno <kelvinotix@gmail.com>
2023-03-23 03:38:16 +0300

    completed task 1 on conditional rendering

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  const[isLoading,setIsLoading] = useState(true)
+  useEffect(()=>{
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000);
+  },[])
+  if(!isLoading){
   return <h2>Multiple Returns Basics</h2>;
+  }
 };
 export default MultipleReturnsBasics;
